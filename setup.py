fbd772048f8ae80aeb6cf3c931ec200e9d7c7025
andrej <zbin.andrej@gmail.com>
2022-10-04 10:41:49 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name="django-json-rpc",
-    version="0.7.2",
+    version="0.7.3",
     description="A simple JSON-RPC implementation for Django",
     author="Samuel Sutch",
     author_email="sam@sutch.net",
@@ -23,18 +23,13 @@ setup(
         'Environment :: Web Environment', 'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent', 'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Framework :: Django', 'Framework :: Django :: 1.4',
-        'Framework :: Django :: 1.5', 'Framework :: Django :: 1.6',
-        'Framework :: Django :: 1.7', 'Framework :: Django :: 1.8'
+        'Framework :: Django', 'Framework :: Django :: 3.2',
     ],
     packages=['jsonrpc'],
     zip_safe=False,  # we include templates and tests
-    install_requires=['Django>=1.0', 'six'],
+    install_requires=['Django>=3.2', 'six'],
     package_data={'jsonrpc': ['templates/*']})
